5731741e431da1efb85beadd6d1dcb685f27cc8a
Anastasiia Shyrokovska <anastasya.shir@gmail.com>
2014-10-03 12:57:03 +0300

    cycles and exceptions

--- src/com/zurmansor/brainfuck/compiler/Compiler.java
+++ src/com/zurmansor/brainfuck/compiler/Compiler.java
@@ -1,13 +1,24 @@
 package com.zurmansor.brainfuck.compiler;
 
+import com.zurmansor.brainfuck.exception.BracketException;
+import com.zurmansor.brainfuck.exception.BrainFuckException;
+import com.zurmansor.brainfuck.exception.UnknownGrammarException;
+
 import java.util.ArrayList;
 
 public class Compiler {
+    private String REGEX = "[+-.\\[\\]<>]*";
+
     private String code;
-    private ArrayList<Operation> operationList = new ArrayList<Operation>();
     private int operationIndex = 0;
+    private int cell = 0;
+    private ArrayList<Operation> operationList = new ArrayList<Operation>();    // масив операций
+    private ArrayList<Integer> chain = new ArrayList<Integer>();        // масив ячеек
+
+    private ArrayList<Integer> result = new ArrayList<Integer>();           //массив вывода
 
     public Compiler() {
+
     }
 
     public Compiler(String code) {
@@ -18,54 +29,54 @@ public class Compiler {
         this.code = code;
     }
 
-//    static public void scan(String code){
-//
-//        ArrayList<Integer> operationList = new ArrayList<Integer>();
-//
-//        for (int i = 0; i < code.length(); i++) {
-//
-//        }
-////        operationList.add();
-//    }
+    public void start() throws BrainFuckException {
+        lexical();
+        compress();
+        semantic();
+        print();
+    }
 
-    public String compress(){
+    private String compress() throws BracketException {
         String codeRes = "";
         int temp = 0;
+        int brackets = 0;
 
         for (int i = 0; i < code.length(); i++) {
             switch (code.charAt(i)) {
                 case '+':
-                    createCellIfEmpty(Operation.Command.ADD);
+                    operationList.add(operationIndex, new Operation(Operation.Command.ADD));
                     i = i + collect('+', i) - 1;
                     break;
 
                 case '-':
-                    createCellIfEmpty(Operation.Command.ADD);
+                    operationList.add(operationIndex, new Operation(Operation.Command.ADD));
                     i = i + collect('-', i) - 1;
                     break;
 
                 case '>':
-                    createCellIfEmpty(Operation.Command.STEP);
+                    operationList.add(operationIndex, new Operation(Operation.Command.STEP));
                     i = i + collect('>', i) - 1;
                     break;
 
                 case '<':
-                    createCellIfEmpty(Operation.Command.STEP);
+                    operationList.add(operationIndex, new Operation(Operation.Command.STEP));
                     i = i + collect('<', i) - 1;
                     break;
 
                 case '[':
-                    createCellIfEmpty(Operation.Command.CYCLE);
-                    i = i + collect('[', i) - 1;
+                    operationList.add(operationIndex, new Operation(Operation.Command.CYCLE, 1));
+                    brackets++;
+//                    i = i + collect('[', i) - 1;
                     break;
 
                 case ']':
-                    createCellIfEmpty(Operation.Command.CYCLE);
-                    i = i + collect(']', i) - 1;
+                    operationList.add(operationIndex, new Operation(Operation.Command.CYCLE, -1));
+                    brackets--;
+//                    i = i + collect(']', i) - 1;
                     break;
 
                 case '.':
-                    createCellIfEmpty(Operation.Command.PRINT);
+                    operationList.add(operationIndex, new Operation(Operation.Command.PRINT));
                     i = i + collect('.', i) - 1;
                     break;
 
@@ -75,17 +86,16 @@ public class Compiler {
 
             }
             operationIndex++;
-//            if (code.charAt(i) == '+') {
-//                int
-//            }
+        }
+
+        if (brackets != 0) {
+            throw new BracketException();
         }
         return codeRes;
     }
 
-    private void createCellIfEmpty(Operation.Command type) {
-        if (operationIndex >= operationList.size()) {
-            operationList.add(operationIndex, new Operation(type));
-        }
+    private void addOperation(Operation.Command type) {
+        operationList.add(operationIndex, new Operation(type));
     }
 
     private int collect (char operation, int i) {
@@ -100,4 +110,153 @@ public class Compiler {
         return operationList.get(operationIndex).getWeightAbs();
     }
 
+    private void lexical() throws UnknownGrammarException {
+//        remove spaces
+        code = code.replaceAll("\\s+","");
+
+        if (!code.matches(REGEX)) {
+            throw new UnknownGrammarException();
+        }
+    }
+
+    private void semantic() {
+        chain.add(0, 0);
+        for (operationIndex = 0; operationIndex < operationList.size(); operationIndex++) {
+            Operation operation = operationList.get(operationIndex);
+
+            switch (operation.getCommand()){
+                case ADD:
+                    // прибавляем вес текущей ячейке
+                    chain.set(cell, chain.get(cell) + operation.getWeight());
+                    break;
+
+                case STEP:
+                    makeStep(operation.getWeight());
+                    break;
+
+                case CYCLE:
+//                    semantic();
+                    if (operation.getWeight() > 0) {
+//                        opening bracket
+                        if (chain.get(cell) <= 0) {
+                            int bracket = 1;
+                            while (bracket > 0) {
+                                operationIndex++;
+                                operation = operationList.get(operationIndex);
+                                if (operation.getCommand() == Operation.Command.CYCLE) {
+                                    bracket += operation.getWeight();
+                                }
+                            }
+//                            operationIndex++;
+                            // если текущая ячейка 0, перейти на операцию последующую за операцией закрывания текущего цикла
+                        }
+                    } else {
+//                        closing bracket
+                        if (chain.get(cell) > 0) {
+                            int bracket = -1;
+                            while (bracket < 0) {
+                                operationIndex--;
+                                operation = operationList.get(operationIndex);
+                                if (operation.getCommand() == Operation.Command.CYCLE) {
+                                    bracket += operation.getWeight();
+                                }
+                            }
+//                            operationIndex++;
+                        }
+                    }
+
+                    break;
+
+                case PRINT:
+//                    System.out.print((char)((int)chain.get(cell)));
+                    // вывести несколько раз, если нужно
+                    int c = operation.getWeight();
+                    while (c > 0) {
+                        result.add(chain.get(cell));
+                        c--;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        return;
+    }
+
+//    private void cycle () {
+//        while (chain.get(cell) > 0) {
+//            int tmpOperationIndex = operationIndex + 1;
+//            Operation tmpOperation = operationList.get(tmpOperationIndex);
+//
+//            while (tmpOperation.getCommand() != Operation.Command.CYCLE || tmpOperation.getWeight() > 0) {
+//
+//                switch (tmpOperation.getCommand()) {
+//                    case ADD:
+//                        // прибавляем вес текущей ячейке
+//                        chain.set(cell, chain.get(cell) + tmpOperation.getWeight());
+//                        break;
+//
+//                    case STEP:
+//                        makeStep(tmpOperation.getWeight());
+//                        break;
+//
+//                    case CYCLE:
+//                        cycle();
+//                        break;
+//
+//                    default:
+//                        break;
+//                }
+//
+//                tmpOperationIndex++;
+//                tmpOperation = operationList.get(tmpOperationIndex);
+//            }
+//        }
+//
+//    }
+
+    private void makeStep (int weight) {
+        // указываем на новую ячейку (смещение)
+        cell += weight;
+
+        if (cell < 0) {
+//          TODO: CATCH
+        }
+
+        // create new and set 0 to missed
+        // задаем 0 новой ячейке и всем ячейкам до нее, если их не было
+        if (cell >= chain.size()) {
+            int tmpCell = chain.size();
+            while (tmpCell <= cell) {
+                chain.add(tmpCell, 0);
+                tmpCell++;
+            }
+        }
+    }
+
+    private void addCellIfEmpty () {
+        if (cell >= chain.size()) {
+            chain.add(cell, 1);
+        }
+    }
+
+    private void printChain () {
+        for (int ch : chain ) {
+            System.out.println(ch);
+        }
+    }
+
+    private void printOperations () {
+        for (Operation operation : operationList ) {
+            System.out.println(operation.getCommand() + "." + operation.getWeight());
+        }
+    }
+
+    private void print () {
+        for (int ch : result) {
+            System.out.print((char)ch);
+        }
+    }
 }
--- src/com/zurmansor/brainfuck/compiler/Main.java
+++ src/com/zurmansor/brainfuck/compiler/Main.java
@@ -1,5 +1,7 @@
 package com.zurmansor.brainfuck.compiler;
 
+import com.zurmansor.brainfuck.exception.BrainFuckException;
+
 import java.util.ArrayList;
 
 public class Main {
@@ -14,19 +16,40 @@ public class Main {
             + "---------------------------------------------"
             + "----.-----------------------.";
 
-    static final String CODE_2 = "+++.>>.+[+-<<<--]]";
+    static final String ABC = "++++++++[>+>++>+++>++++>+++++>++++++>+++++++>++++++++>+++++++++>++++++++++>+++++++++++>++++++++++++>+++++++++++++>++++++++++++++>+++++++++++++++>++++++++++++++++<<<<<<<<<<<<<<<<-]>>>>>>>>>>>>+.-<<<<<<<<<<<<>>>>>>>>>>>>++.--<<<<<<<<<<<<>>>>>>>>>>>>+++.---<<<<<<<<<<<<.";
+    static final String HELLO = "+++++ +++++ [->++ +++++ +++<] >++++ .---. +++++ ++..+ ++.<";
+
+    static final String CODE_2 = "+++.>>>.+[+-<<--]]";
+    static final String CODE_3 = "+++.>++.>++++><<---";
+    static final String CODE_4 = "+++>++[>+++<-]>>+++++++";
+    static final String CODE_5 = "++>++[>+++[>+<-]<-]>>>++++";
+    static final String CODE_6 = "++++++>+++[<++>-]";
+    static final String CODE_7 = "+++++ ++++[ ->+++ +++++ +<]>+ +++++ +++++ +++++ ...+. ..+.+ +.++. <+++[->+++ <]>++ .++.< +++[- >---< ]>--- .<+++ ++++[ ->--- ----< ]>--- -.++.<++++ [->-- --<]> ----- .<+++ +[->+ +++<] >++++ +++.. <";
+    static final String CODE_8 = ">+>+>+>+>++<[>[<+++>->>>>>>+>+>+>+>++<[>[<+++>->>>>>>+>+>+>+>++<[>[<+++>->>>>>>+>+>+>+>++<[>[<+++>->>>>>+++[->+++++<]>[-]<<<<<<]<<]>[-]<<<<<]<<]>[-]<<<<<]<<]>[-]<<<<<]<<]>.";
+
+    static final String CODE_9 = "erfer";
 
 
     public static void main(String[] args) {
-        System.out.println(CODE_2);
-
         ArrayList<Integer> al = new ArrayList<Integer>();
 
-        Compiler compiler = new Compiler(CODE_2);
-        compiler.compress();
+        Compiler compiler = new Compiler(HELLO);
+        try {
+            compiler.start();
+        } catch (BrainFuckException e) {
+            e.printStackTrace();
+        }
 
-
-    }
+//        String data = "23343453";
+//        String data2 = "+++-[]].";
+//        String regex = "[\\D]+";
+//        String regex2 = "[+-.\\[\\]]*";
+//
+//        String data3 = "acbg";
+//        String regex3 = "[abc]*";
+//
+//        System.out.println(data2.matches(regex2));
+            }
 
 
 
